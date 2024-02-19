36de9d9473b2e1e9d625bdb7a56cedef79caf62a
akshaykhanapuri <akshaykhanapuri12@gmail.com>
2024-02-19 20:55:28 +0530

    Update segment.ts

--- src/lib/segment/segment.ts
+++ src/lib/segment/segment.ts
@@ -1,11 +1,6 @@
 import { AnalyticsBrowser } from "@segment/analytics-next";
 import { envClient } from "../envClient";
-import { Analytics } from "@segment/analytics-node";
 
 export const analytics = AnalyticsBrowser.load({
-  writeKey: envClient.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
-});
-
-export const analyticsServer = new Analytics({
   writeKey: envClient.NEXT_PUBLIC_SEGMENT_WRITE_KEY,
 });
